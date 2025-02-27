bd69447ecd330aa95423f8e1ec5d96e13ca34da3
syqwq <3422403944@qq.com>
2025-02-27 20:41:50 +0800

    unfinish

--- Feb-27/4.cpp
+++ Feb-27/4.cpp
@@ -1,33 +1,91 @@
 #include <iostream>
+#include <cstdio>
 #define rep(i,a,b) for(int i=a;i<=b;i++)
 #define per(i,a,b) for(int i=a;i>=b;i--)
 using namespace std;
 
 const int N=1010;
+const int inf=0x7fffffff;
 
 int n,m;
 int a[N],b[N];
 int k;
 
+double max(double x,double y){return x>y?x:y;}
+
+int bs(int arr[],int len,int x){
+    int l=1,r=len;
+    while(l<r){
+        int mid=(l+r+1)>>1;
+        if(arr[mid]<=x) l=mid;
+        else r=mid-1;
+    }
+    if(arr[l]>x) return l-1;
+    return l;
+}
+
+int fk(int kk){
+    int l=1,r=n;
+    while(l<r){
+        int la=(l+r+1)>>1; int lb=k-la;
+        int mid=a[la]>b[lb]?a[la]:b[lb];
+        if(bs(a,n,mid)+bs(b,m,mid)<=k) l=la;
+        else r=la-1;
+    }
+    return max(a[l],b[kk-l]);
+}
+
 
 int main(){
+    a[0]=-inf,b[0]=-inf;
     cin>>n; rep(i,1,n) cin>>a[i];
     cin>>m; rep(i,1,m) cin>>b[i];
     k=(n+m+1)>>1;
 
-
     int l=1,r=n;    // on len of a
-    while(l<r){
-        int la=(l+r)>>1; int lb=k-la;
-        if(a[la]<b[lb-1]) l=la+1;
-        else r=la;
+    if((n+m)%2==1){
+        while(l<r){
+            int la=(l+r+1)>>1; int lb=k-la;
+            int mid=a[la]>b[lb]?a[la]:b[lb];
+            if(bs(a,n,mid)+bs(b,m,mid)<=k) l=la;
+            else r=la-1;
+        }
+        printf("%.5f",(double)max(a[l],b[k-l]));
+    }else{
+        while(l<r){
+            int la=(l+r+1)>>1; int lb=k+1-la;
+            int mid=a[la]>b[lb]?a[la]:b[lb];
+            
+            // printf("k=%d la=%d lb=%d\nl=%d r=%d mid=%d\nbs(a,n,%d)+bs(b,m,%d)=%d\n",k,la,lb,l,r,mid,mid,mid,bs(a,n,mid)+bs(b,m,mid));
+            
+            
+            if(bs(a,n,mid)+bs(b,m,mid)<=k+1) l=la;
+            else r=la-1;
+        }
+        double bb=max(a[l],b[k+1-l]);
+        l=1,r=n;
+        while(l<r){
+            int la=(l+r+1)>>1; int lb=k-la;
+            int mid=a[la]>b[lb]?a[la]:b[lb];
+            if(bs(a,n,mid)+bs(b,m,mid)<=k) l=la;
+            else r=la-1;
+        }
+        double aa=max(a[l],b[k-l]);
+        printf("%.5f",(aa+bb)/2);
+        
+        // printf("a[l]=%d\n",a[l]);
+        // if(a[l]>b[k+1-l]){
+            // double aa=a[l],bb=max(a[l-1],b[k+1-l]);
+            // // printf("aa=%.5f bb=%.5f\n",aa,bb);
+            // printf("%.5f",(aa+bb)/2);
+        // }else{
+            // double aa=b[k+1-l],bb=max(a[l],b[k-l]);
+            // // printf("aa=%.5f bb=%.5f\n",aa,bb);
+            // printf("%.5f",(aa+bb)/2);
+        // }
+
     }
 
-    int la=l,lb=k-l;
-    if((n+m)%2==1) cout<<max(a[la],b[lb]);
-    else cout<<(double)((double)a[la]+(double)b[lb])/2;
-    // 1 2 5
-    // 3 4
 
 
     return 0;
---
+++ Feb-27/5 copy.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define per(i,a,b) for(int i=a;i>=b;i--)
+using namespace std;
+
+const int N=5010;
+
+int n;
+int a[N];
+int tar;
+
+int bs(int arr[],int _l,int _r,int x){
+    int l=_l,r=_r;
+    while(l<r){
+        int mid=(l+r+1)>>1;
+        if(arr[mid]<=x) l=mid;
+        else r=mid-1;
+    }
+    if(a[l]==x) return l;
+    return 0;
+}
+
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    cin>>n;
+    rep(i,1,n) cin>>a[i];
+    cin>>tar;
+
+    int l=1,r=n;
+    while(l<r){
+        int mid=(l+r+1)>>1;
+        if(a[mid]>a[n]) l=mid;
+        else r=mid-1;
+    }
+    if(tar>a[n]) cout<<bs(a,1,l,tar)-1;
+    else cout<<bs(a,l+1,n,tar)-1;
+
+
+    return 0;
+}
